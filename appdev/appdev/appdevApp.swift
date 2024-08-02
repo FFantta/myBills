e59d97c91db7e7e0b06ced56af153d69274c098b
scyjc8 <junrun.chen@outlook.com>
2024-08-02 08:48:49 +0800

    First init

---
+++ appdev/appdev/appdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  appdevApp.swift
+//  appdev
+//
+//  Created by 俊润 Chen on 2024/8/2.
+//
+
+import SwiftUI
+
+@main
+struct appdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
